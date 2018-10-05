0fb0b3e771e84588dcfce7dd848a9edabffff13c
Robyn <Robyn@nothsadmins-MacBook-Pro.local>
2018-10-05 22:58:21 +0100

    Count score and refactor

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ flags = {
 
 var index, allCountries, allFlags, currentCountry, currentFlag;
 
+var score;
+
 var CCOUNT = 300; // 5 minutes
     
 var t, count;
@@ -26,16 +28,33 @@ var setUpEventListeners = function() {
         }
     });
     document.querySelector('.start__button').addEventListener('click', function () {
-        document.querySelector('.text__box').style.display = 'block';
-        document.querySelector('.timer').style.display = 'block';
-        document.querySelector('.start__button').style.display = 'none';
-        document.querySelector('.flag__outline').style.display = 'block';
-        document.querySelector('.enter__guess').focus();
-        document.querySelector('.buttons').style.display = 'block';
-        
+        setUp();
+    
+        setGameDisplay();
         
     });
 };
+
+function setGameDisplay() {
+    document.querySelector('.text__box').style.display = 'block';
+    document.querySelector('.timer').style.display = 'block';
+    document.querySelector('.start__button').style.display = 'none';
+    document.querySelector('.winner').style.display = 'none';
+    document.querySelector('.flag__outline').style.display = 'block';
+    document.querySelector('.enter__guess').focus();
+    document.querySelector('.buttons').style.display = 'block';
+    document.querySelector('.score').style.display = 'block';
+    
+    score = 0;
+    document.querySelector('.score').textContent = score;
+    focusInputTextBox()
+}
+
+function setUp() {
+    allCountries = Object.keys(flags);
+    allFlags = Object.values(flags);
+    nextQuestion();
+}
 
 function reset() {
     timerReset();
@@ -43,16 +62,18 @@ function reset() {
 }
 
 function skip() {
-    nextQuestion()
-    document.querySelector('.enter__guess').value = "";
-    document.querySelector('.enter__guess').focus();
+    nextQuestion() // prevent next question from being same flag
+    focusInputTextBox()
+
 }
 
 function displayTimer() {
     var minutes = Math.floor(count / 60);
-    minutes = String("0" + minutes).slice(-2);
     var seconds = count % 60;
+    
+    minutes = String("0" + minutes).slice(-2);
     seconds = String("0" + seconds).slice(-2);
+    
     var countdown = minutes + ':' + seconds;
 
     document.getElementById('timespan').innerHTML = countdown;
@@ -62,6 +83,7 @@ function countdown() {
     displayTimer();
     if (count == 0) {
         console.log('time is up')
+        // TODO
     } else {
         count--;
         t = setTimeout("countdown()", 1000);
@@ -79,41 +101,16 @@ function timerReset() {
 };
 
 
-init();
-
-function init() {
-    allCountries = Object.keys(flags)
-    allFlags = Object.values(flags)
-    index = randomIndex(allCountries);
-    currentCountry = allCountries[index];
-    currentFlag = allFlags[index];
-
-    document.querySelector('.flag__image').src = 'assets/' + currentFlag;
-    document.querySelector('.text__box').style.display = 'None';
-    document.querySelector('.flag__outline').style.display = 'none';
-    document.querySelector('.buttons').style.display = 'none';
-    document.querySelector('.timer').style.display = 'none';
-    document.querySelector('.start__button').style.display = 'block';
-    // document.querySelector('.timer').value = '10.00';
-
-
-    setUpEventListeners();
-}
-
-
-
-
-
-
 function game() {
     var correct = checkGuess();
     if (correct) {
+        score++;
+        document.querySelector('.score').textContent = score;
         if (allCountries.length === 1) {
             console.log('winner');
             finishGame();
         } else {
-            document.querySelector('.enter__guess').value = "";
-            document.querySelector('.enter__guess').focus();
+            focusInputTextBox()
 
             removeCountry();
             nextQuestion();
@@ -123,17 +120,19 @@ function game() {
 };
 
 function finishGame() {
-    // note time
-    // change display
-    // add to leaderboad
-    // leaderboard = [{score:, time:}, {etc.}]
+    document.querySelector('.winner').style.display = 'block';
+    hideQuestionDisplay()
+    timerReset();
+
+    document.querySelector('.start__button').value = 'new game';
+    document.querySelector('.start__button').style.display = 'block';
 }
 
-
-
-function checkGuess() {
-    var ans = document.querySelector('.enter__guess').value.toLowerCase();
-    return ans === currentCountry ?  true :  false;
+function hideQuestionDisplay() {
+    document.querySelector('.flag__outline').style.display = 'none';
+    document.querySelector('.text__box').style.display = 'none';
+    document.querySelector('.timer').style.display = 'none';
+    document.querySelector('.buttons').style.display = 'none';
 }
 
 function nextQuestion(){
@@ -141,6 +140,16 @@ function nextQuestion(){
     currentCountry = allCountries[index];
     currentFlag = allFlags[index];
     document.querySelector('.flag__image').src = 'assets/' + currentFlag
+}
+
+function checkGuess() {
+    var ans = document.querySelector('.enter__guess').value.toLowerCase();
+    return ans === currentCountry ?  true :  false;
+}
+
+function focusInputTextBox() {
+    document.querySelector('.enter__guess').value = "";
+    document.querySelector('.enter__guess').focus();
 }
 
 function removeCountry() {
@@ -153,3 +162,15 @@ function randomIndex(list) {
     return Math.floor(Math.random() * len);
 }
 
+function init() {
+    hideQuestionDisplay()
+    
+    document.querySelector('.start__button').style.display = 'block';
+    document.querySelector('.start__button').value = 'Start';
+    document.querySelector('.score').style.display = 'none';
+    document.querySelector('.winner').style.display = 'none';
+    
+    setUpEventListeners();
+}
+
+init();
