8c0d8a1cf65590777309b625deed9c72ee378bf2
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-10-09 11:37:32 +0100

    Add webpack config for optimizing build before distribution

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      favicon: './src/favicon.ico',
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new CopyPlugin([{ from: './src/images', to: 'images' }]),
+    new ImageminPlugin()
+  ]
+};
